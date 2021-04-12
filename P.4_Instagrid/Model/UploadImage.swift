9fbb926a4478171b28a58d45c55528d5d37cc828
Fardi Issihaka <fardi.issihaka@laposte.net>
2021-04-13 00:06:45 +0200

    project init

---
+++ P.4_Instagrid/Model/UploadImage.swift
@@ -0,0 +1,17 @@
+//
+//  UploadImage.swift
+//  P.4_Instagrid
+//
+//  Created by Fard iOS on 20/03/2021.
+//
+
+import Foundation
+
+import UIKit
+
+
+// MARK: - Logique de l'application (l'ajout des photos et partage avec les amis)
+
+class UploadImage {
+    
+}
